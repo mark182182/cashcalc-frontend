26dd0659e2c7828b0aa28eca395ad614d4ec2b7f
mark182182 <versalife11@hotmail.com>
2020-02-23 22:08:28 +0100

    feat(app.component.html): modal window for login and calculate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import { faWeightHanging } from '@fortawesome/free-solid-svg-icons';
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
-import { faShieldAlt } from '@fortawesome/free-solid-svg-icons';
-import { faShippingFast } from '@fortawesome/free-solid-svg-icons';
+import {
+  faWeightHanging,
+  faDollarSign,
+  faShieldAlt,
+  faShippingFast,
+  faUser,
+  faKey,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -11,34 +15,38 @@ import { faShippingFast } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CashCalc';
+  isActive: boolean = false;
+  menuStatus: boolean = false;
+  loginMenu: boolean = false;
+
+  title: String = 'CashCalc';
   faGlobe = faGlobe;
   faWeightHanging = faWeightHanging;
   faDollarSign = faDollarSign;
   faShieldAlt = faShieldAlt;
   faShippingFast = faShippingFast;
+  faUser = faUser;
+  faKey = faKey;
 
-  ngOnInit() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0
-    );
+  ngOnInit() {}
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+  toggleMenu = () => {
+    this.menuStatus = !this.menuStatus;
+  };
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-        });
-      });
-    }
-  }
+  openLogin = () => {
+    this.loginMenu = true;
+  };
+
+  closeLogin = () => {
+    this.loginMenu = false;
+  };
+
+  openCalculateWindow = () => {
+    this.isActive = true;
+  };
+
+  closeCalculateWindow = () => {
+    this.isActive = false;
+  };
 }
