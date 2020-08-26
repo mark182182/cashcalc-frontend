3fa47aa9261d52835197352b3455d5408d5167b1
mark182182 <versalife11@hotmail.com>
2020-08-26 04:13:51 +0200

    fix(request): dispatch error pages to history

--- src/request/request.js
+++ src/request/request.js
@@ -8,27 +8,4 @@ const request = axios.create({
   withCredentials: true,
 });
 
-request.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    switch (error.response.status) {
-      case 401:
-        history.push('/login');
-        break;
-      case 403:
-        history.push('/main/403');
-        break;
-      case 404:
-        history.push('/main/404');
-        break;
-      case 500:
-        history.push('/main/500');
-        break;
-      default:
-        break;
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default request;
--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,8 @@ import { createBrowserHistory } from 'history';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from '../redux-reducer/root';
+import { push } from 'connected-react-router';
+import request from '../request/request';
 
 const persistConfig = {
   key: 'root',
@@ -29,6 +31,29 @@ export default (preloadedState) => {
   );
 
   const persistor = persistStore(store);
+
+  request.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      switch (error.response.status) {
+        case 401:
+          store.dispatch(push('/login'));
+          break;
+        case 403:
+          store.dispatch(push('/main/403'));
+          break;
+        case 404:
+          store.dispatch(push('/main/404'));
+          break;
+        case 500:
+          store.dispatch(push('/main/500'));
+          break;
+        default:
+          break;
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return { store, persistor };
 };
