2d80e9edeb3ac7963f1deae147da1692b7148c91
mark182182 <versalife11@hotmail.com>
2020-09-07 10:57:24 +0200

    fix(request): increase timeout from 5 to 15000

--- src/request/request.js
+++ src/request/request.js
@@ -3,7 +3,7 @@ import constants from '../constants/constants';
 
 const request = axios.create({
   baseURL: constants.BASE_URL,
-  timeout: 5000,
+  timeout: 15000,
   withCredentials: true,
 });
 
