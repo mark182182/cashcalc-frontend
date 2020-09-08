b613c5308510891b2256143d9c243e56c59e9d94
mark182182 <versalife11@hotmail.com>
2020-09-08 09:59:42 +0200

    fix(timeout): increase to 30sec

--- src/request/request.js
+++ src/request/request.js
@@ -3,7 +3,7 @@ import constants from '../constants/constants';
 
 const request = axios.create({
   baseURL: constants.BASE_URL,
-  timeout: 15000,
+  timeout: 30000,
   withCredentials: true,
 });
 
